4059a532acd7692135ac2ca8620071ff1d3bca22
ApoorvShri <lcb2021008@iiitl.ac.in>
2023-07-17 01:06:29 +0530

    Added Exercise detail page

---
+++ src/components/SimilarExercises.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import HorizontalScrollBar from "./HorizontalScrollbar";
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
+  return (
+    <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
+      <Typography variant="h3">
+        Exercises that target the same muscle group
+      </Typography>
+
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {targetMuscleExercises.length && (
+          <HorizontalScrollBar data={targetMuscleExercises} />
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default SimilarExercises;
